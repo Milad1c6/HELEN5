8b2bfccb864c07024e1494357c0784d12372c41a
Milad1c6 <7090mil@gmail.com>
2026-09-08 01:33:46 +0330

    Update login.js

--- login/login.js
+++ login/login.js
@@ -1,1 +1,32 @@
+const form = document.getElementById("loginForm");
 
+form.addEventListener("submit", function (event) {
+
+    event.preventDefault();
+
+    const username = document
+        .getElementById("username")
+        .value
+        .trim();
+
+    const password = document
+        .getElementById("password")
+        .value
+        .trim();
+
+    // هر چیزی وارد شود قبول می‌شود
+    if (username === "" || password === "") {
+        return;
+    }
+
+    // ذخیره اسم
+    localStorage.setItem(
+        "loveUsername",
+        username
+    );
+
+    // رفتن به انفجار
+    window.location.href =
+        "../explosion/explosion.html";
+
+});
